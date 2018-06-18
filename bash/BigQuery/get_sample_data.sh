1ac1010d62288a5a751cdd2ce052a5a108e1c994
Kazuya Araki <lakeel.l0545@gmail.com>
2018-06-18 14:22:36 +0900

    csv -> tsv対応

--- bash/BigQuery/get_sample_data.sh
+++ bash/BigQuery/get_sample_data.sh
@@ -2,7 +2,7 @@
 
 # const.
 SQL_LIST=./sql_list.txt
-RESULT=./result.csv
+RESULT=./result.tsv
 
 # init RESULT
 cat /dev/null > ${RESULT}
@@ -10,5 +10,5 @@ cat /dev/null > ${RESULT}
 # get sample data for BigQuery
 while read row;
   do
-    echo `bq query --nouse_legacy_sql --format csv "${row}" | sed "2 d"` >> ${RESULT}
+    echo `bq query --nouse_legacy_sql --format csv "${row}" | sed "2 d"` | tr "," "\\t" >> ${RESULT}
   done < ${SQL_LIST}
